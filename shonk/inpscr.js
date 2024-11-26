cfc68cfce6e085bb82d69f6cf2d6e4138945c7f1
foreseenlime <troyhkiwi@gmail.com>
2024-11-27 11:20:46 +1300

    added an upgrade system

--- shonk/inpscr.js
+++ shonk/inpscr.js
@@ -1,20 +1,43 @@
 const count = document.getElementById("COUNT");
 const btn = document.getElementById("bnt");
 const nug = document.getElementById("nug");
+const purch = document.getElementById("buy")
+
+
 var num = 0;
 var canAdd = true;
 var hun = 100;
+var upgradeCost = 1;
 
+updateStats();
+
+//pet shonk
 btn.onclick = function() {
   
   if (canAdd === true) {
     canAdd = false;
     num += 1;
-    count.textContent = num;
+    updateStats();
     
     setTimeout(function() {
       canAdd = true;
-    }, 1000);
+    }, 1);
     
   };
 };
+
+purch.onclick = function() {
+  if (num >= upgradeCost) {
+    num -= upgradeCost;
+    upgradeCost *= 2;
+    updateStats();
+  };
+};
+
+function updateStats() {
+  count.textContent = `${num} XP`;
+  nug.textContent = hun;
+  purch.textContent = upgradeCost;
+}
+
+
